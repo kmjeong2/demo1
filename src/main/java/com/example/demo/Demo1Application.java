0b6dd6d03284c39ab5f2bdcbed1112c058f1e041
owner <owner@my>
2019-11-13 00:33:18 +0900

    gfggg

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
 	public static void main(String[] args) {
-		int a;
+		int asddc2;
 		System.out.println("wssse");
 		SpringApplication.run(Demo1Application.class, args);
 	}
